259a4cda25cbed5f4bd3ca7626305e1e8e180514
Elena17T <torgasheva17elena@gmail.com>
2023-03-01 01:22:55 +0200

    added folder fake-data and displayed some components

--- week1/project/ecomerce/src/App.js
+++ week1/project/ecomerce/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import products from './fake-data/all-products'
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title"><h1>Products</h1></div>
+      
+    {products.map((product, id) => {
+    return(
+      <div key = {id}>
+        <div className = "productDescription">
+          <a href = ""> <img src={product.image}></img></a>
+          <a href=""><p>{product.title}</p></a>
+      </div>
+      </div>
+    // <div key = {id}>
+    //         <p>{product.title}</p>
+    //         <p>{product.price}</p>
+    //         <p>{product.description}</p>
+    //         <p>{product.category}</p>
+    //         <p>{product.image}</p>
+    //         <p>{product.rating}</p>
+    // </div>
+    )}
+    )}
     </div>
   );
 }
+
 
 export default App;
