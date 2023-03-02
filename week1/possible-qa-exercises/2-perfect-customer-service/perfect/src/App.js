a0f9604a26d59c3a24b9727270d2851091dda67d
Elena17T <torgasheva17elena@gmail.com>
2023-03-02 22:52:22 +0200

    created files for the second exersice

---
+++ week1/possible-qa-exercises/2-perfect-customer-service/perfect/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import items from './Guarantee';
+
+function App() {
+  return(
+    <div>
+      {items.map((item, index) => {
+        return(
+          <div key = {index}>
+            <div>
+            <img src={item.image}></img>
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+ 
+}
+
+export default App;
---
+++ week1/possible-qa-exercises/2-perfect-customer-service/perfect/src/Guarantee.js
@@ -0,0 +1,20 @@
+
+
+
+export default [
+    {
+        image: "require(./images/sakura.jpg)",
+        title: "mountain",
+        description: "beautiful mountain in the evening",
+    },
+    {
+        image: "./images/forest.jpg",
+        title: "rock",
+        description: "dangerous rock in the Atlantic ocean",
+    },
+    {
+        image: "./images/nature.jpg",
+        title: "house",
+        description: "empty house in the main avenue",
+    },
+]
