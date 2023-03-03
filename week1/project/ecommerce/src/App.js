814089544c8e781a90e621e7a75f5cc805a454ac
svetikzp <s.ostryanskaya@gmail.com>
2023-03-03 15:01:56 +0100

    add file

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -2,22 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import CategoryList from './components/categories';
 import products from './fake-data/all-products';
-import categories from './fake-data/all-categories';
 import Products from './components/products';
 
-function App () {
-  const [state, setState] = useState({selectedCategory:'', isAll: true})
+function App() {
   return (
     <div className="App">
-     <h1>
-      Products
-     </h1>
-{
-  CategoryList ({categories, state, setState})
-}
-{
-  Products(products, state)
-}
+      <h1>
+        Products
+      </h1>
+      {CategoryList ()}
+      {Products(products)
+      }
     </div>
   );
 }
