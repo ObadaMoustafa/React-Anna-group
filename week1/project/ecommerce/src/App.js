2cc2a3b18e8fbdab572c309a1692232ed2119ff0
svetikzp <s.ostryanskaya@gmail.com>
2023-03-03 11:38:46 +0100

    Add to projet

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -1,20 +1,22 @@
-//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import CategoryList from "./components/categories"
-import products from "./fake-data/all-products"
+import CategoryList from './components/categories';
+import products from './fake-data/all-products';
+import categories from './fake-data/all-categories';
 import Products from './components/products';
 
 function App () {
+  const [state, setState] = useState({selectedCategory:'', isAll: true})
   return (
     <div className="App">
      <h1>
       Products
      </h1>
 {
-  CategoryList () 
+  CategoryList ({categories, state, setState})
 }
 {
-  Products(products)
+  Products(products, state)
 }
     </div>
   );
--- week1/project/ecommerce/src/components/categories.js
+++ week1/project/ecommerce/src/components/categories.js
@@ -1,4 +1,5 @@
-import './categories.css';
+/*import './categories.css';
+
 import categoryaltern from "../fake-data/all-categories" 
 function Category (prop) {
     return (
@@ -12,5 +13,36 @@ function CategoryList  (prop) {
         }
     </div>
     )
-}export default
- CategoryList;
+}*/
+import './categories.css';
+
+const Category = (props) => {
+    const {current, state, setState} = props
+    const key = current.slice(6)
+
+    const className = 'category-item' + ((key === state.selectedCategory) && !state.isAll ? ' category-selected' : '')
+
+    const changeFilter = (key) => {
+        setState(prevState => {
+            return {
+                selectedCategory: key, 
+                isAll: (prevState.selectedCategory === key) && !prevState.isAll
+            }
+        })
+    }
+
+    return (
+        <div key={key} className={className} onClick={() => changeFilter(key)}>{current}</div>
+    )
+}
+
+const CategoriesList = (props) => {
+    const {categories, state, setState} = props;
+    return (
+        <div className="categories">
+            {categories.map(current => Category({ current, state, setState }))}
+        </div>
+    )
+}
+
+export default CategoryList;
