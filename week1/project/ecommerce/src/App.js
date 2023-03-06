a70c2f4fa5c996836b1d639a2104e8a4393e6262
svetikzp <s.ostryanskaya@gmail.com>
2023-03-06 23:30:32 +0100

    Add files

---
+++ week1/project/ecommerce/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+import categories from './fake-data/all-categories';
+import products from './fake-data/all-products';
+import CategoryList from './components/categories';
+import ProductsList from './components/products';
+
+function App() {
+  const [state, setState] = useState({sortCategory:'', anyAll: true})
+  return (
+    <div className="App">
+      <h1>Products</h1>
+      { CategoryList({categories, state, setState}) }
+      { ProductsList({products, state}) }
+    </div>
+  );
+}
+
+export default App;
---
+++ week1/project/ecommerce/src/components/categories.jsx
@@ -0,0 +1,32 @@
+import './categories.css';
+
+const Category = (props) => {
+    const {current, state, setState} = props
+    const key = current.slice(6)
+
+    const className = 'category--item' + ((key === state.sortCategory) && !state.anyAll ? 'category-sort' : '')
+
+    const changeFilter = (key) => {
+        setState(prevState => {
+            return {
+                sortCategory: key, 
+                anyAll: (prevState.sortCategory === key) && !prevState.anyAll
+            }
+        })
+    }
+
+    return (
+        <div key={key} className={className} onClick={() => changeFilter(key)}>{current}</div>
+    )
+}
+
+const CategoryList = (props) => {
+    const {categories, state, setState} = props;
+    return (
+        <div className="categories">
+            {categories.map(current => Category({ current, state, setState }))}
+        </div>
+    )
+}
+
+export default CategoryList;
