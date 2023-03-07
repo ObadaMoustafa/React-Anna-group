77a011ef7c6a23595a9bc5bab75554d3f022d51a
Admin <admin@Admins-MacBook-Pro.local>
2023-03-07 23:08:05 +0200

    set products filter by category

--- week1/project/ecommerce/src/components/product-list.jsx
+++ week1/project/ecommerce/src/components/product-list.jsx
@@ -1,16 +1,21 @@
 import products from "../fake-data/all-products";
 import Product from "./product";
 import "./product.css";
-function ProductList() {
+
+function ProductList(props) {
+  const activeCategory = props;
+  const getFilter = activeCategory === "All products" ? "" : activeCategory;
+//   console.log(getFilter);
+  const filterName = getFilter.activeCategory;
+//   console.log(filterName);
+  const filteredList = filterName
+    ? products.filter((product) => product.category === filterName)
+    : products;
+
   return (
     <ul className="products-list">
-      {products.map((item) => {
+      {filteredList.map((item) => {
         const { id, image, title } = item;
-        //   if (props.activeCategory !== "All products") {
-        //       const filteredProducts = products.filter((product) => {
-        //           return
-        //       })
-        //   }
 
         return <Product key={id} title={title} img={image} />;
       })}
@@ -18,5 +23,27 @@ function ProductList() {
   );
 }
 export default ProductList;
+
+///////////////////////////////////////////////////////////////////////
+// Пример отображения с Product from Product List
+///////////////////////////////////////////////////////////////////////
+// function ProductList(props) {
+
+//     return (
+//       <ul className="products-list">
+//         {products.map((item) => {
+//           const { id, image, title } = item;
+//           //   if (props.activeCategory !== "All products") {
+//           //       const filteredProducts = products.filter((product) => {
+//           //           return
+//           //       })
+//           //   }
+
+//           return <Product key={id} title={title} img={image} />;
+//         })}
+//       </ul>
+//     );
+// }
+// export default ProductList;
 
 // activeCategory;
