6177099918005b8a50328dfa63f46e19a5222069
Admin <admin@Admins-MacBook-Pro.local>
2023-03-11 14:45:09 +0200

    resized photos and added string before fetching

--- week2/possible-qa-exercises/1-dog-photo/src/components/DogGallery.jsx
+++ week2/possible-qa-exercises/1-dog-photo/src/components/DogGallery.jsx
@@ -21,7 +21,7 @@ function DogGallery() {
       .then((response) => response.json())
       // .then((data) => console.log(data))
       .then((data) => setDogPhotos([...dogPhotos, data]));
-    
+
     // .then((data) =>
     //   setDogPhotos((prevState) => {
     //     prevState.push(data);
@@ -32,23 +32,22 @@ function DogGallery() {
   return (
     <div>
       <Button onClick={getDogPhoto} text="Get a dog!" />
-
-      <ul>
-        {dogPhotos.map((item) => {
-          return (
-            <li key={item.id}>
-              <DogPhoto img={item.message} />
-              {item.message}
-            </li>
-          );
-        })}
-      </ul>
+      {dogPhotos.length === 0 ? (
+        <p>Get your first dog by clicking the button!</p>
+      ) : (
+        <ul>
+          {dogPhotos.map((item) => {
+            const dogName = item.message.slice(30);
+            return (
+              <li key={item.id}>
+                <DogPhoto img={item.message} />
+                {dogName}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
 export default DogGallery;
-
-// 6. Inside `<DogGallery>` return all the dogs stored in the `dogPhotos` array using the `map()` function. Pass down each `dogPhoto` into an instance of `<DogPhoto>`.
-
-// 3. Inside (before the return statement) also create a function called `getDogPhoto`. The purpose of this function is to make an API call to `https://dog.ceo/api/breeds/image/random`. You can either use `fetch` or `axios`. At the end of the function push the new `dog image URL` into the `dogPhotos` array state variable using `setDogPhotos`
-// 5. Pass down the function `getDogPhoto` to `<Button>`
--- week2/possible-qa-exercises/1-dog-photo/src/components/DogPhoto.jsx
+++ week2/possible-qa-exercises/1-dog-photo/src/components/DogPhoto.jsx
@@ -1,7 +1,7 @@
 function DogPhoto({ img }) {
   return (
     <div>
-      <img src={img} alt="dogphoto" />
+      <img src={img} alt="dogphoto" width="150" />
     </div>
   );
 }
